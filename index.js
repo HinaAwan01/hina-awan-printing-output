c719e95450c8308e02040ab8523fde5e9a821062
Hina Awan <65899846+HinaAwan01@users.noreply.github.com>
2020-05-27 23:22:44 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+//   Author: HINA SHAFIQUE AWAN
+
+
+/********************  START OF TASK 1 *****************************/
+
+let a = 3;
+let b = 5;
+let c;
+
+let output; 
+// task description
+output = 'let a = 3;\n';
+output += 'let b = 5;\n';
+output += 'let c;\n';
+output += '---------\n';
+
+// using operators
+
+output += 'a + b = ' + (a + b) + '\n';
+
+output += 'a - b = ' + (a - b) + '\n';
+
+output += 'a * b = ' + (a * b) + '\n';
+
+output += 'a / b = ' + (a / b) + '\n';
+
+output += 'a % b = ' + (a % b) + '\n';
+
+output += 'a += b = ' + (a += b) + '\n';
+
+output += 'a -= b = ' + (a -= b) + '\n';
+
+output += 'a *= b = ' + (a *= b) + '\n';
+
+output += 'a /= b = ' + (a /= b) + '\n';
+
+output += 'a  %= b = ' + (a  %= b) + '\n';
+
+output += 'a == b = ' + (a == b) + '\n';
+
+output += 'a != b = ' + (a != b) + '\n';
+
+output += 'a > b = ' + (a > b) + '\n';
+
+output += 'a < b = ' + (a < b) + '\n';
+
+output += '!a && !c = ' + (!a && !c) + '\n';
+
+output += '!a || !c = ' + (!a || !c) + '\n';
+
+
+alert(output);
+
+
+/********************  END OF TASK 1 *****************************/
+
+
+/********************  START OF TASK 2 *****************************/
+
+//Declare the variable first_name and assign it with the value of your first name;
+
+let first_name = "Hina";
+
+
+//Declare the variable last_name and assign it with the value of your last name;
+
+let last_name = "Awan";
+
+//Declare the variable email and assign it with the value of your email;
+
+let email = "hina_awan01@yahoo.com";
+
+//Declare the variable output;
+
+let output2;  //since variable output is already declared in Task 1, so I have created vaiable output2
+
+
+//Create the expression concatenating variables first_name, last_name, email and string literals needed to complete the sentence reading: "My name is Your-first-name Your-last-name. You can contact me at your-email@mail.com.";
+
+output2= "My name is " + first_name + " " +  last_name + "." +  "You can contact me at " + email + ".";
+
+
+//Print the output in JavaScript console.
+
+console.log(output2);
+
+/********************  END OF TASK 2 *****************************/
